600439b64a56d7ef3eecc9f2adefea6b7bf15d3b
Lee <rhkd4129@naver.com>
2022-03-24 06:14:30 +0900

    'passwordchanging..

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import get_object_or_404, redirect, render
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm,PasswordChangeForm
 from .forms import SignupForm,ProfileEditForm
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required 
+
 def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
@@ -27,3 +28,13 @@ def profile_edit(request):
         form = ProfileEditForm(instance = request.user)
     return render(request,'accounts/profile_edit.html',{'form':form})
 
+@login_required
+def password_change(request):  
+    if request.method =='POST':
+        form =  PasswordChangeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('blog:post_list')
+    else:
+        form = PasswordChangeForm()
+    return render(request,'accounts/profile_edit.html',{'form':form})
